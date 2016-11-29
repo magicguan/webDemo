b8e9c5d6a10653b196b5477837496bcfad00d945
magicguan <guanguanma@gmail.com>
2016-11-29 15:58:54 +0800

    11/28

---
+++ src/main/java/org/nsfw/webDemo/config/TestConfig.java
@@ -0,0 +1,18 @@
+package org.nsfw.webDemo.config;
+
+/**
+ * function description.
+ * <p>
+ * <p><h2>Descriptions</h2>
+ * <h3>Project</h3> webDemo
+ * <h3>Package</h3> org.nsfw.webDemo.config
+ * </p>
+ * <p><h2>Change History</h2>
+ * 2016/11/29 15:57 | G | created
+ * </p>
+ *
+ * @author G
+ * @version 1.0.0
+ */
+public class TestConfig {
+}
